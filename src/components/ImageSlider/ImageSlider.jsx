507350309c66546bf307be9f1d58be2bcc277b66
roverflow <roverflow5@gmail.com>
2021-05-20 03:17:13 +0530

    Updated Bad dependencies
    Fixed by removing react-fa package

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Carousel, { arrowsPlugin, autoplayPlugin  } from '@brainhubeu/react-carousel';
+import Carousel, {  autoplayPlugin  } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import imageOne from './1.jpg'
 import imageTwo from './2.jpg'
@@ -11,22 +11,13 @@ import imageSeven from './7.jpg'
 import imageEight from './8.jpg'
 import imagenine from './9.jpg'
 import imageten from './10.jpg'
-import Icon from 'react-fa';
+
 
 const ImageSlider = () => (
   
   <Carousel plugins={[
+    'arrows',
     'infinite',
-    {
-      resolve: arrowsPlugin,
-      options: {
-        arrowLeft: <button><Icon name="angle-double-left" /></button>,
-        arrowLeftDisabled:<button><Icon name="angle-left" /></button>,
-        arrowRight: <button><Icon name="angle-double-right" /></button>,
-        arrowRightDisabled: <button><Icon name="angle-right" /></button>,
-        addArrowClickHandler: true,
-      }
-    },
     {
       resolve: autoplayPlugin,
       options: {
