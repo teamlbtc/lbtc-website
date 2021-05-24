8e77ec6fdc7009840fd5aa6888b55e1c329cdb62
roverflow <roverflow5@gmail.com>
2021-05-24 22:14:30 +0530

    Changed popup Contents
    Added different Content for pop-up

--- src/components/popup/NewPop.jsx
+++ src/components/popup/NewPop.jsx
@@ -1,44 +1,41 @@
-import React , { useState } from 'react'
-import './popup.css'
-import 'reactjs-popup/dist/index.css';
-import { Fab } from '@material-ui/core';
+import React, { useState } from "react";
+import "./popup.css";
+import "reactjs-popup/dist/index.css";
 
+function NewPop() {
+  const [Buttonpopup, setButtonpopup] = useState(true);
 
-function NewPop(props) {
-
-    const [Buttonpopup , setButtonpopup] = useState(true);
-
-    
-    
-    return (Buttonpopup) ? (
-        
-        <div className="popup" onClick={() => setButtonpopup(false)}>
-            <div className="popup-inner">
-                <button className="close-btn" onClick={() => setButtonpopup(false)}>X</button>
-                <div className="content">
-                    {' '}
-                    <h1>Ketto Crowdfunding</h1>
-                    <h3>#WeStandTogether - Oxygen Support for 10K+ Vulnerable Families</h3>
-                    <br />
-                    The aim of the project is to provide emergency oxygen support for all the 10K Vulnerable Families in all the 6 urban poor settlements, where we are conducting 'Covid19 Relief Project' from the past 8 months in association with Azim Premji Foundation (APF), BBMP & Govt. of Karnataka.
-                    <br />
-                    <br />
-                    <a
-                        class="thm-btn yellowcolor"
-                        href="https://www.ketto.org/fundraiser/westandtogether-oxygen-support-for-10k-vulnerable-families?payment=form"
-                        title=""
-                        itemprop="url"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={{ textDecoration: "none"}}
-                    >Click to know more</a>
-                </div>
-                { props.children }
-            </div>
+  return Buttonpopup ? (
+    <div className="popup" >
+      <div className="popup-inner">
+        <button className="close-btn" onClick={() => setButtonpopup(false)}>
+          X
+        </button>
+        <div className="content">
+          <h1>#WeStandTogether</h1>
+          <h3>
+            Join our campaign on Ketto to contribute in helping 10000+ vulnerable families in Bangalore as a part of our COVID-19 Relief Project.
+          </h3>
+          {/* <br />
+          The aim of the project is to provide emergency oxygen support for all the 10K Vulnerable Families in all the 6 urban poor settlements, where we are conducting 'Covid19 Relief Project' from the past 8 months in association with Azim Premji Foundation (APF), BBMP & Govt. of Karnataka.
+          <br /> */}
+          <br />
+          <a class="thm-btn yellowcolor"
+            href="https://www.ketto.org/fundraiser/westandtogether-oxygen-support-for-10k-vulnerable-families?payment=form"
+            title=""
+            itemprop="url"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            Click to know more
+          </a>
         </div>
-    ) 
-    :
-    "";
+      </div>
+    </div>
+  ) : (
+    ""
+  );
 }
 
-export default NewPop
+export default NewPop;
