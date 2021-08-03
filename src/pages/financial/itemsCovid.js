e043c76e1f79e813f3f0ff95141a0c5fb89bda2e
roverflow <roverflow5@gmail.com>
2021-08-03 13:41:55 +0530

    Added Funds page
    added new funds page

---
+++ src/pages/financial/itemsCovid.js
@@ -0,0 +1,30 @@
+export const CovidRelif = [
+  {
+    image: require("../../assets/images/covidrp/2.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/covidrp/3.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/covidrp/4.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/covidrp/5.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/covidrp/6.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/covidrp/7.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/covidrp/8.jpg"),
+    clss: "image",
+  },
+];
---
+++ src/pages/financial/itemsWe.js
@@ -0,0 +1,34 @@
+export const Imageswe = [
+  {
+    image: require("../../assets/images/we/1Weslide1.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/we/2Weslide2.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/we/3Weslide3.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/we/4Weslide4.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/we/5Weslide5.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/we/6Weslide6.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/we/7Weslide7.jpg"),
+    clss: "image",
+  },
+  {
+    image: require("../../assets/images/we/8Weslide8.jpg"),
+    clss: "image",
+  },
+];
